cb6f92955bdb9f17aea56a69ccaa8b341986977d
thisisommore <ommore501@gmail.com>
2023-03-25 14:54:54 +0000

    Try sizeof and string input

---
+++ size_of.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int t;
+  cout << "Input - ";
+  cin >> t;
+  int max[t];
+  cout << sizeof(max) / sizeof(*max) << endl;
+}
---
+++ str_inp.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main() {
+  char max[10];
+  printf("str is %s\n", max);
+  scanf("%s", max);
+  printf("str is %s\n", max);
+}
