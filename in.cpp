514878613846e195bb01ac00bce0900d44fdcaa2
thisisommore <ommore501@gmail.com>
2023-04-01 05:18:27 +0000

    Use namespace

---
+++ in.cpp
@@ -0,0 +1,7 @@
+#include "namespace.h"
+using namespace max;
+int main() {
+  int t = 10;
+  max_pro();
+  printf("%d\n", t);
+}
---
+++ namespace.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+namespace max {
+void max_pro() { cout << "We r max pru" << endl; }
+} // namespace max
